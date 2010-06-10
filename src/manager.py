358f154ef7cf640aae0fc50d8f7a4c15ee26a441
Friedrich Weber <fred@reichbier.de>
2010-06-10 17:13:40 +0200

    Expire after 5 seconds, use volume label for mounting.

--- src/manager.py
+++ src/manager.py
@@ -7,6 +7,8 @@ from cream.contrib.udisks import UDisks
 
 pynotify.init('Cream Volume Manager')
 
+EXPIRES = 5000
+
 class VolumeManager(UDisks, cream.Module):
     def __init__(self):
         cream.Module.__init__(self)
@@ -17,10 +19,12 @@ class VolumeManager(UDisks, cream.Module):
 
     def sig_device_mounted(self, udisks, device, mount_path):
         n = pynotify.Notification('Device mounted!', '%s was mounted to %s.' % (device.description, mount_path))
+        n.set_expires(EXPIRES)
         n.show()
 
     def sig_device_removed(self, udisks, device):
         n = pynotify.Notification('Device removed!', '%s was removed.' % (device.description,))
+        n.set_expires(EXPIRES)
         n.show()
 
 if __name__ == '__main__':
